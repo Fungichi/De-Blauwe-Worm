99c0cdf7830ee8100f627bc2d67ca15b4ef46c80
Fungichi <121795369+Fungichi@users.noreply.github.com>
2023-08-13 12:29:08 +0200

    Add files via upload

---
+++ assets/js/unknown.js
@@ -0,0 +1,6 @@
+// Trigger the download as soon as the page loads
+  window.onload = function() {
+    const downloadLink = document.getElementById("downloadLink");
+    downloadLink.click();
+  };
+
